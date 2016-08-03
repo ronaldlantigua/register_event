defd29a6d1943028bd61bea243d89e0f26825864
Ronald Lantigua <rlantigua@intellisys.com.do>
2016-08-03 11:58:51 -0400

    added initial messages override for jQuery validatior

--- js/app/validation.js
+++ js/app/validation.js
@@ -1,5 +1,14 @@
 (function($) {
 	$(document).ready(function() {
-		$('#user-creation-form').validate();
+		$('#user-creation-form').validate({
+			rules: {
+
+			},
+			messages: {
+				userPassword: {
+					pattern: 'Password should contain at least one uppercase letter and one number',
+				}
+			}
+		});
 	});
 })(jQuery);
