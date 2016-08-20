fab130e635e6106cb36a61a3fc116cebf88ad279
Ronald Lantigua <ronaldlantigua@gmail.com>
2016-08-20 16:10:53 -0400

    validating event start and end date

--- js/app/validation.js
+++ js/app/validation.js
@@ -12,10 +12,48 @@ function userCreationValidation(){
 	});
 }
 
+function isDateGreaterThanToday(date) {
+	var splitDate = startDate.split('T');
+	var month = splitDate[0];
+	var day = splitDate[1];
+	var year = splitDate[2];
+	var splitTime = splitDate[1].split(':');
+	var hour = parseInt(splitTime[0]);
+}
+
+jQuery.validator.addMethod('endate_greater_startdate', function(value, element) {
+	var startDate = jQuery('#event-start-date').val();
+	var endDate = jQuery('#event-end-date').val();
+	startDate = new Date(startDate);
+	endDate = new Date(endDate);
+    return endDate > startDate;
+}, 'Enddate should be greater than Startdate');
+
+jQuery.validator.addMethod('date_greater_than_now', function(value, element, params) {
+	var date = jQuery(params).val();
+	var date1 = date.split('T')[0];
+	var splitDate = date1.split('-');
+	var time = date.split('T')[1];
+	var splitTime = time.split(':');
+	var year = parseInt(splitDate[0]);
+	var month = (parseInt(splitDate[1])) - 1;
+	var day = parseInt(splitDate[2]);
+	var hours = parseInt(splitTime[0]);
+	var minutes = parseInt(splitTime[1]);
+	date = new Date(year, month, day, hours, minutes, 0, 0);
+    return date > new Date();
+}, 'date should be greater than now');
+
 function eventCreationValidation(){
 	return jQuery('#event-creation-form').validate({
 		rules: {
-
+			startDate: {
+				date_greater_than_now: '#event-start-date'
+			},
+			endDate: {
+				endate_greater_startdate: true,
+				date_greater_than_now: '#event-end-date'
+			}
 		},
 		messages: {
 			eventGuest: {
